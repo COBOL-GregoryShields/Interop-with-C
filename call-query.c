7de6cc4bdfcd413862f10b760d79debd141d057e
Laptop Mac <gs@gregoryshields.net>
2023-09-12 20:31:41 -0700

    Minor edits to call-query.c file.

--- call-query.c
+++ call-query.c
@@ -7,14 +7,14 @@ int main (int argc, char * argv [])
     char     Buffer [1024];
     size_t   Size = sizeof (Buffer);
 
-    if (0 == Query (Buffer, &Size))
+    if (0 == Query(Buffer, &Size))
     {
         printf ("failed to call Query\n");
     }
     else
     {
         char * Ptr = Buffer;
-        while (Size-- > 0) putchar (*Ptr++);
+        while (Size-- > 0) putchar(*Ptr++);
         putchar ('\n');
     }
 }
